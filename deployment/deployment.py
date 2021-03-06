f32977fc3bbc0680d05aa6e6f136d8fa6f614e75
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2021-03-06 15:26:22 +0000

    feature: cluster deployment

--- deployment/deployment.py
+++ deployment/deployment.py
@@ -1,8 +1,6 @@
 from spores.Clusters import clusters_create
-import os
 
-directory = os.path.abspath("./clusters")
-
+directory = "./clusters"
 
 clusters_create(directory, delete_if_exists=True, init_script_path="./init_scripts")
 
